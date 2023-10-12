b8ae97a859b75f1c42207f00adc318f74d77986c
TrueChainz <adnan.aant@gmail.com>
2023-10-12 09:26:07 +0100

    feat: added new vim keymap to go straight to remap file

--- .config/nvim/lua/truechainz/remap.lua
+++ .config/nvim/lua/truechainz/remap.lua
@@ -1,7 +1,7 @@
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>", { desc = "[P]roject [V]iew" })
+vim.keymap.set("n", "<leader>pv", "<cmd>Ex!<CR>", { desc = "[P]roject [V]iew" })
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -10,7 +10,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set({ "n", "v" }, "<leader>fy", [["+y]], { desc = "[F]ile Clipboard [P]aste" })
 vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, { desc = "[F]ile [F]ormat" })
 vim.keymap.set("n", "<leader>fc", "<cmd>so<CR>", { desc = "[F]ile [C]ompile" })
-vim.keymap.set("n", "<leader>fs", "<cmd>w<CR>", { desc = "[F]ile [S]ave " })
+vim.keymap.set("n", "<leader>fs", "<cmd>w!<CR>", { desc = "[F]ile [S]ave " })
 vim.keymap.set("n", "<leader>fq", "<cmd>q<CR>", { desc = "[F]ile [Q]uit " })
 vim.keymap.set("n", "<leader>fr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
   { desc = "[F]ile word [R]eplace all" })
@@ -38,4 +38,6 @@ vim.keymap.set("n", "<leader>rj", "<cmd>lprev<CR>zz", { desc = "[R]esults Prev" 
 
 
 vim.keymap.set("n", "<leader>vl", "<cmd>e ~/.dotfiles/.config/nvim/lua/truechainz/lazy.lua<CR>",
-  { desc = "[v]im [l]azy" })
+  { desc = "[V]im [L]azy" })
+vim.keymap.set("n", "<leader>vr", "<cmd>e ~/.dotfiles/.config/nvim/lua/truechainz/remap.lua<CR>",
+  { desc = "[V]im [R]emaps" })
